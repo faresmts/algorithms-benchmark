1dda86983b677f3193af6e32ec34559bdb9a6d53
fares <matheusfarz@gmail.com>
2025-06-20 10:17:39 -0300

    feat: add results for 1k

--- main.cpp
+++ main.cpp
@@ -5,12 +5,10 @@
 
 int main() {
 
-    AlgorithmType algorithm_type = AlgorithmType::SORTING;
-    size_t vector_size = 10000;
-    TestCaseType test_case = TestCaseType::RANDOM;
-
-    size_t execution_count = 1;
-
+    AlgorithmType algorithm_type = AlgorithmType::SELECTION;
+    size_t vector_size = 1000000;
+    TestCaseType test_case = TestCaseType::NEARLY_SORTED;
+    size_t execution_count = 1000;
 
     for (size_t i = 0; i <= execution_count; i++) {
         try {
